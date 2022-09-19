16b926777976b94417284b3f5e30cf9c6f2d9e14
Roshan John <91770248+RoshanJ20@users.noreply.github.com>
2022-09-19 15:30:49 +0530

    fourth commit

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import re
 def display_hash(hashtable) -> None:
 	for keyvalue in range(len(hashtable)):
 		cont = hashtable[keyvalue]
-		print(keyvalue, end = "")
+		print(keyvalue, end = " ")
 		for element in cont:
-			print(" --> ", element, end = "", sep = "")
+			print("-->", element, end = " ")
 		print()
 
 def Hashing(keyvalue) -> int:
